5fa9d494df9fabf604768f7b5e793e1a3abf74ff
Candice Peters <candicelizabethpeters@gmail.com>
2019-08-19 06:19:21 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,19 @@
-def hello_t
-
+def hello_t(array)
+  if block_given? #if a block was given
+  i=0 #same as counter
+  while i < array.length 
+  yield array[i]#we are yielding each item of the array to the block below 
+  i += 1 
+  end
+  array #returns out original array
+else
+  puts "Hey! No block was given!" #if no block is given
+end
 end
 
-# call your method here!
-
+#method array items are being yielded too with argument
+hello_t(["Tim", "Tom", "Jim"]) do |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+end
