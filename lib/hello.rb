8a37227938a8882ada43207c5d49823a541d04a4
cep104 <candicelizabethpeters@gmail.com>
2020-11-09 14:41:14 -0600

    Done.

--- lib/hello.rb
+++ lib/hello.rb
@@ -13,6 +13,11 @@ def hello_t(array)
     end
   end
 
+  #look at tests for what how the informaton is getting yielded
+  #You've already worked with enumerator methods like #each and #collect. 
+  #These methods are called on collections, such as arrays. 
+  #They take blocks as their arguments and yield each element of the collection to the block, allowing the code in the block to be applied to each element of the collection.
+
 # hello_t(["Tim", "Tom", "Jim"]) do |name|
 #     if name.start_with?("T")
 #       puts "Hi, #{name}"
